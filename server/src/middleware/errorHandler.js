5b5e03e2d52a3a8cb13961f0fc9fd0e333066f0e
Ganta Mohan Chaitanya <chaitanyaganta01@gmail.com>
2026-08-16 00:31:24 +0530

    express server with mongo connection and errorhandling implemented

---
+++ server/src/middleware/errorHandler.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    message: err.message || "Something Went Wrong",
+  });
+};
+
+export default errorHandler;
